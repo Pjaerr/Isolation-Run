5cd813d07fa2fe8fd73958aa333c299044857001
Pjaerr <joshcharlesjackson@gmail.com>
2020-03-26 14:32:25 +0000

    Redirect to https
    If the url is http, redirect to https. I am doing this clientside as if http is forced it doesn't pose a security risk, just the websockets won't work.

--- frontend/public/build/bundle.js
+++ frontend/public/build/bundle.js
@@ -2974,6 +2974,11 @@ var app = (function () {
     	}
     }
 
+    //Redirect here as if http is forced it doesn't really pose a security risk and doing in node causing issues.
+    if (location.protocol === "http:") {
+      location.replace("https://" + location.host);
+    }
+
     const app = new App({
       target: document.body
     });
